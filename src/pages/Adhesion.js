d84325c90b0cf055ce7fe7aa63374923591c95c3
Silvio VASCONCELOS <svasconcelos.prestataire@oui.sncf>
2021-09-14 17:56:47 +0200

    9th commit

--- src/pages/Adhesion.js
+++ src/pages/Adhesion.js
@@ -1,16 +1,41 @@
-import React, {Component} from "react";
+import React, {useState} from "react";
 import ficheInscription from "../ficheInscription.jpeg"
+import {Button, Image, Modal} from "react-bootstrap";
 
-class Adhesion extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Inscriptions</h1>
-                <p>Pour vous inscrire veuillez à télécharger et remplir ce document :</p>
-                <a href={ficheInscription} download>Lien pour la fiche d'inscription</a>
-            </div>
-        );
-    }
+
+function Adhesion() {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    return (
+        <div>
+            <h1>Inscriptions</h1>
+
+            <p>Pour vous inscrire veuillez à télécharger et remplir la fiche d'inscription</p>
+
+            <Button variant="dark" size="sm" onClick={handleShow}>
+                Afficher la fiche d'inscription
+            </Button>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Fiche d'inscription</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p>Pour télécharger la fiche d'inscription faites clic droit puis "Enregistrez l'image sous..."</p>
+                    <Image src={ficheInscription} fluid></Image>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Fermer
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
+        </div>
+    );
+
 }
 
 export default Adhesion;
