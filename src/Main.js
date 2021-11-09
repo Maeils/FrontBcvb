7ac5949d43bac078ea6b608f084e89f5bcf46af4
Maeils <silvio.vasc3@gmail.com>
2021-11-09 23:40:36 +0100

    Changement d'un site multipage à un mono page avec condition de state

--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,4 @@
 import React, {Component} from "react";
-import {
-    Route,
-    HashRouter
-} from "react-router-dom";
 import Navbar from 'react-bootstrap/Navbar'
 import NavDropdown from 'react-bootstrap/NavDropdown'
 import Nav from 'react-bootstrap/Nav'
@@ -19,40 +15,70 @@ import Contact from "./pages/Contact";
 import Merch from "./pages/Merch";
 
 class Main extends Component {
+
+    constructor(){
+        super();
+        this.state = {Club : true};
+    }
+
+    changeComponentToDisplay(newComponent: string){
+        this.changeBooleanToFalse();
+        switch(newComponent){
+            case "Club":  this.setState({Club: true}) ;break;
+            case "Adhesion":  this.setState({Adhesion: true}); break;
+            case "Junior":  this.setState({Junior: true}); break;
+            case "Loisirs":  this.setState({Loisirs: true}); break;
+            case "Competition":  this.setState({Competition: true}); break;
+            case "Contact":  this.setState({Contact: true}); break;
+            case "Merch":  this.setState({Merch: true}); break;
+            default : this.setState({Club: true});
+        }
+    }
+
+    changeBooleanToFalse(){
+        this.setState({Club: false})
+        this.setState({Adhesion: false})
+        this.setState({Junior: false})
+        this.setState({Loisirs: false})
+        this.setState({Competition: false})
+        this.setState({Contact: false})
+        this.setState({Shop: false})
+    }
+
+
   render() {
       return (
-        <HashRouter>
+      <div>
           <Navbar collapseOnSelect expand="lg" bg="black" variant="dark">
             <Container>
-            <Navbar.Brand href="/"><Image width={50} height={50} src={logo} roundedCircle/></Navbar.Brand>
+            <Navbar.Brand href="/" onClick={() =>this.changeComponentToDisplay("Club")}><Image width={50} height={50} src={logo} roundedCircle/></Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link href="#/club">Le club</Nav.Link>
-                <Nav.Link href="#/adhesion">Adhésion 2021/2022</Nav.Link>
+                <Nav.Link onClick={() =>this.changeComponentToDisplay("Club")}>Le club</Nav.Link>
+                <Nav.Link onClick={() =>this.changeComponentToDisplay("Adhesion")}>Adhésion 2021/2022</Nav.Link>
                 <NavDropdown menuVariant="dark" title="Séances" id="navbarScrollingDropdown">
-                  <NavDropdown.Item href="#/jeunes">Jeunes</NavDropdown.Item>
-                  <NavDropdown.Item href="#/loisirs">Loisirs</NavDropdown.Item>
-                  <NavDropdown.Item href="#/competitions">Compétition</NavDropdown.Item>
+                  <NavDropdown.Item onClick={() =>this.changeComponentToDisplay("Junior")}>Jeunes</NavDropdown.Item>
+                  <NavDropdown.Item onClick={() =>this.changeComponentToDisplay("Loisirs")}>Loisirs</NavDropdown.Item>
+                  <NavDropdown.Item onClick={() =>this.changeComponentToDisplay("Competition")}>Compétition</NavDropdown.Item>
                 </NavDropdown>
-                <Nav.Link href="#/contact">Contact</Nav.Link>
-                <Nav.Link href="#/shop">Le shop</Nav.Link>
+                <Nav.Link onClick={() =>this.changeComponentToDisplay("Contact")}>Contact</Nav.Link>
+                <Nav.Link onClick={() =>this.changeComponentToDisplay("Merch")}>Le shop</Nav.Link>
 
                             </Nav>
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
                 <Container>
-                    <Route exact path="/" component={Club}/>
-                    <Route path="/club" component={Club}/>
-                    <Route path="/adhesion" component={Adhesion}/>
-                    <Route path="/jeunes" component={Junior}/>
-                    <Route path="/loisirs" component={Loisirs}/>
-                    <Route path="/competitions" component={Competition}/>
-                    <Route path="/contact" component={Contact}/>
-                    <Route path="/shop" component={Merch}/>
+                    {this.state.Club && <Club/>}
+                    {this.state.Adhesion && <Adhesion/>}
+                    {this.state.Junior && <Junior/>}
+                    {this.state.Loisirs && <Loisirs/>}
+                    {this.state.Competition && <Competition/>}
+                    {this.state.Contact && <Contact/>}
+                    {this.state.Merch && <Merch/>}
                 </Container>
-            </HashRouter>
+                </div>
         );
     }
 }
